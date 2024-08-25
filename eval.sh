d1de003723896bf5b5de26ea0f650fffd1007722
mateuszb <mateusz.b@aibusters.com>
2024-08-25 18:21:51 +0200

    test

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 CUDA_VISIBLE_DEVICES=0
 python eval.py --model log/log_aoanet_rl/model.pth \
     --infos_path log/log_aoanet_rl/infos_aoanet.pkl \
+    --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
     --language_eval 1 \
