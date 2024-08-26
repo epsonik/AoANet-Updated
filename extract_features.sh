953da343cba8a9afcefe191cee590a7bf97dbca0
mateuszb <mateusz.b@aibusters.com>
2024-08-27 00:26:08 +0200

    test

--- extract_features.sh
+++ extract_features.sh
@@ -1,3 +1,3 @@
-python scripts/prepro_feats.py --input_json /home2/data/images/coco2014/karpathy/dataset_coco.json \
+python scripts/prepro_feats.py --input_json '/home2/data/images/coco2014/karpathy/dataset_coco.json' \
 --output_dir data/cocotalk \
 --images_root '/home2/data/images/coco2014/'
