99180691c9b40f9dba8c5d9f41c468aaf631f676
mateuszb <mateusz.b@aibusters.com>
2024-11-02 12:28:36 +0100

    remove unsed imports

--- print_model.py
+++ print_model.py
@@ -9,21 +9,21 @@ from torch import nn
 # print(torchinfo.summary(modules, (3, 224, 224), batch_dim=0,
 #                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 # #
-print("resnet")
-resnet152 = torchvision.models.resnet152(weights="IMAGENET1K_V2")
-print(torchinfo.summary(resnet152, (3, 224, 224), batch_dim=0,
-                        col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+# print("resnet")
+# resnet152 = torchvision.models.resnet152(weights="IMAGENET1K_V2")
+# print(torchinfo.summary(resnet152, (3, 224, 224), batch_dim=0,
+#                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 # resnet101 = torchvision.models.resnet101(pretrained=True)
 # print(torchinfo.summary(resnet101, (3, 224, 224), batch_dim=0,
 #                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 #
-# print("dense")
+print("dense121")
 # densenet161 = torchvision.models.densenet161(pretrained=True)
 # print(torchinfo.summary(densenet161, (3, 224, 224), batch_dim=0,
 #                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
-# densenet121 = torchvision.models.densenet121(pretrained=True)
-# print(torchinfo.summary(densenet121, (3, 224, 224), batch_dim=0,
-#                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
+densenet121 = torchvision.models.densenet121(pretrained=True)
+print(torchinfo.summary(densenet121, (3, 224, 224), batch_dim=0,
+                        col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
 # densenet201 = torchvision.models.densenet201(pretrained=True)
 # print(torchinfo.summary(densenet201, (3, 224, 224), batch_dim=0,
 #                         col_names=("input_size", "output_size", "num_params", "kernel_size", "mult_adds"), verbose=0))
