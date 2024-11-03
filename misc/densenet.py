35a61c75e1e1f1a22a98b2fc3ea4f015e669ac51
mateuszb <mateusz.b@aibusters.com>
2024-11-03 23:43:14 +0100

    densenet121

--- misc/densenet.py
+++ misc/densenet.py
@@ -6,11 +6,11 @@ class DenseNet(torchvision.models.densenet.DenseNet):
         super(DenseNet, self).__init__(block, layers, num_classes)
         maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0,
                                     ceil_mode=True) # change
-        getattr(self,'features')['pool0'] = maxpool
+        getattr(self,'features').pool0 = maxpool
         # fine tune 2, 3, 4
-        for i in range(2, 4):
-            getattr(self.features, 'denseblock%d'%i).conv1.stride = (2,2)
-            getattr(self.features, 'denseblock%d'%i).conv2.stride = (1,1)
+        for i in range(2, 5):
+            getattr(self.features, 'denseblock%d'%i)[0].conv1.stride = (2,2)
+            getattr(self.features, 'denseblock%d'%i)[0].conv2.stride = (1,1)
 
 def densenet121(pretrained=False):
     model = DenseNet(32, (6, 12, 24, 16))
