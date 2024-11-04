10bfb89e0c9117028cdc2c964452c6f5c603b950
mateuszb <mateusz.b@aibusters.com>
2024-11-04 13:51:42 +0100

    densenet121

--- misc/densenet.py
+++ misc/densenet.py
@@ -6,8 +6,6 @@ class DenseNet121(nn.Module):
     def __init__(self):
         super(DenseNet121, self).__init__()
         densenet121 = torchvision.models.densenet121(pretrained=True).cuda()
-        modules = list(densenet121.children())[:-1]
-        densenet121 = nn.Sequential(*modules)
         maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0,
                                ceil_mode=True)  # change
         getattr(densenet121, 'features').pool0 = maxpool
@@ -16,4 +14,8 @@ class DenseNet121(nn.Module):
         for i in range(2, 5):
             getattr(densenet121.features, 'denseblock%d' % i).denselayer1.conv1.stride = (2, 2)
             getattr(densenet121.features, 'denseblock%d' % i).denselayer1.conv2.stride = (1, 1)
+
+        modules = list(densenet121.children())[:-1]
+        densenet121 = nn.Sequential(*modules)
+
         self.model = densenet121
