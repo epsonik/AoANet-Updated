a4b7ab150d30cee374bdd96dcda93d6cd3954198
mateuszb <mateusz.b@aibusters.com>
2024-11-07 23:27:15 +0100

    densenet201

--- scripts/prepro_feats.py
+++ scripts/prepro_feats.py
@@ -69,7 +69,6 @@ def main(params):
   print("Loading images")
   for i,img in enumerate(imgs):
     # load the image
-    print(img['filename'])
     I = skimage.io.imread(os.path.join(params['images_root'], img['filepath'], img['filename']))
     # handle grayscale  input images
     if len(I.shape) == 2:
