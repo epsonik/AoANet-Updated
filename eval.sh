0d3b00429090bf7683f4cf571db6316c4f98d9f8
mateuszb <mateusz.b@aibusters.com>
2024-11-24 22:58:51 +0100

    save checkpoints every iter

--- eval.sh
+++ eval.sh
@@ -1,11 +1,15 @@
 CUDA_VISIBLE_DEVICES=0
-python -u eval.py --model log/log_aoanet_rl/model.pth \
-    --infos_path log/log_aoanet_rl/infos_aoanet.pkl \
-    --dump_images 0 \
-    --dump_json 1 \
-    --num_images -1 \
-    --language_eval 1 \
-    --beam_size 3 \
-    --batch_size 400 \
-    --split test \
-    --cnn_model densenet161 \
+#!/bin/bash
+for i in $(seq 285000 3000 453000)
+do
+  python -u eval.py --model log/log_aoanet_rl/model-$i.pth \
+      --infos_path log/log_aoanet_rl/infos_aoanet-$i.pkl \
+      --dump_images 0 \
+      --dump_json 1 \
+      --num_images -1 \
+      --language_eval 1 \
+      --beam_size 3 \
+      --batch_size 400 \
+      --split test \
+      --cnn_model densenet161 \
+done
