509aac58f695a828ca2119e142fd3ddb48abdac5
mateuszb <mateusz.b@aibusters.com>
2024-12-29 20:32:08 +0100

    save checkpoints every iter

--- train_org.sh
+++ train_org.sh
@@ -19,11 +19,11 @@ python -u train.py --id $id \
     --label_smoothing 0.2 \
     --input_json data/cocotalk.json \
     --input_label_h5 data/cocotalk_label.h5 \
-    --input_fc_dir  data/inception_fc \
-    --input_att_dir  data/inception_att  \
+    --input_fc_dir  data/densenet121_fc \
+    --input_att_dir  data/densenet121_att  \
     --input_box_dir  data/cocobu_box \
-    --fc_feat_size 2048 \
-    --att_feat_size 2048 \
+    --fc_feat_size 1024 \
+    --att_feat_size 1024 \
     --seq_per_img 5 \
     --batch_size 10 \
     --beam_size 1 \
@@ -58,11 +58,11 @@ python -u train.py --id $id \
     --dropout_aoa 0.3 \
     --input_json data/cocotalk.json \
     --input_label_h5 data/cocotalk_label.h5 \
-    --input_fc_dir  data/inception_fc \
-    --input_att_dir  data/inception_att  \
+    --input_fc_dir  data/densenet121_fc \
+    --input_att_dir  data/densenet121_att  \
     --input_box_dir  data/cocobu_box \
-    --fc_feat_size 2048 \
-    --att_feat_size 2048 \
+    --fc_feat_size 1024 \
+    --att_feat_size 1024 \
     --seq_per_img 5 \
     --batch_size 10 \
     --beam_size 1 \
