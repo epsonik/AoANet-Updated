9ca4d12d15b3662b667bf47a912b449cefbe93a5
mateuszb <mateusz.b@aibusters.com>
2025-05-26 15:33:33 +0200

    save checkpoints every iter

--- eval.sh
+++ eval.sh
@@ -1,11 +1,12 @@
 CUDA_VISIBLE_DEVICES=0
-python -u eval.py --model log/log_aoanet_rl/model-best.pth \
-    --infos_path log/log_aoanet_rl/infos_aoanet.pkl \
+
+python -u eval.py --model log/old/resnet152/log_aoanet/model-best.pth \
+    --infos_path log/old/resnet152/log_aoanet/infos_aoanet.pkl \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
     --language_eval 1 \
-    --beam_size 4 \
+    --beam_size 3 \
     --batch_size 400 \
     --split test \
     --cnn_model regnet16 \
