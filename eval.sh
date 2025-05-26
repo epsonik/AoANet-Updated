98ff57806f50e3f0fead674dab2f49bfeaf7d0e4
mateuszb <mateusz.b@aibusters.com>
2025-05-26 17:02:13 +0200

    save checkpoints every iter

--- eval.sh
+++ eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0
-for b in resnet152 resnet101 regnet inception densenet161 densenet121 densenet201
+for b in resnet152 resnet101 regnet inception densenet161 densenet121 densenet201;
 do
-for k in 1 2 3 5 8
+for k in 1 2 3 5 8;
 do
 python -u eval.py --model log/old/$b/log_aoanet/model-best.pth \
     --infos_path log/old/$b/log_aoanet/infos_aoanet.pkl \
@@ -14,6 +14,6 @@ python -u eval.py --model log/old/$b/log_aoanet/model-best.pth \
     --beam_size $k \
     --batch_size 40 \
     --split test \
-    --cnn_model regnet16 \
-done
+    --cnn_model regnet16 ;
+done;
 done
