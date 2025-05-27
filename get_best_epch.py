4f0a2e134826e81aca74f01b777c7d4a599fb9bf
mateuszb <mateusz.b@aibusters.com>
2025-05-28 00:13:20 +0200

    save checkpoints every iter

--- get_best_epch.py
+++ get_best_epch.py
@@ -7,14 +7,35 @@ import argparse
 import misc.utils as utils
 
 
-infos_path="log/old/inception/log_aoanet/infos_aoanet-best.pkl"
-infos={}
-with open(infos_path, 'rb') as f:
-    infos = utils.pickle_load(f)
-print(infos_path)
-print(infos.get('epoch', 0))
-# for k in vars(infos['opt']).keys():
-#     print(k)
-#     print(getattr(infos['opt'], k, ''))
+infos_paths=["log/old/resnet152/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/resnet101/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/regnet/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/inception/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/densenet201/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/densenet161/log_aoanet/infos_aoanet-best.pkl",
+            "log/old/densenet121/log_aoanet/infos_aoanet-best.pkl"]
+infos_paths_rl=["log/old/resnet152/log_aoanet_rl/infos_aoanet-best.pkl",
+               "log/old/resnet101/log_aoanet_rl/infos_aoanet-best.pkl",
+            "log/old/regnet/log_aoanet_rl/infos_aoanet-best.pkl",
+            "log/old/inception/log_aoanet_rl/infos_aoanet-best.pkl",
+            "log/old/densenet201/log_aoanet_rl/infos_aoanet-best.pkl",
+            "log/old/densenet161/log_aoanet_rl/infos_aoanet-best.pkl",
+            "log/old/densenet121/log_aoanet_rl/infos_aoanet-best.pkl"]
+
+for x in infos_paths:
+
+    infos={}
+    with open(x, 'rb') as f:
+        infos = utils.pickle_load(f)
+    print(x)
+    print(infos.get('epoch', 0))
+
+for x in infos_paths_rl:
+    infos = {}
+    with open(x, 'rb') as f:
+        infos = utils.pickle_load(f)
+    print(x)
+    print(infos.get('epoch', 0))
+
 
 
