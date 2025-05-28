a4b9cafb613ce322198a27d97f34f0b2d95e3c14
mateuszb <mateusz.b@aibusters.com>
2025-05-28 23:43:12 +0200

    save checkpoints every iter

--- eval.sh
+++ eval.sh
@@ -8,7 +8,7 @@ python -u eval.py --model log/old/$b/log_aoanet/model-best.pth \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
-    --name inception$bSSHHHHHHHHHHHHH$k \
+    --name regnet$bSSHHHHHHHHHHHHH$k \
     --language_eval 1 \
     --beam_size $k \
     --batch_size 40 \
