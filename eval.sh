87f095bcd793a1dbcb859cc1734b586eec34ef50
mateuszb <mateusz.b@aibusters.com>
2025-05-28 23:51:33 +0200

    save checkpoints every iter

--- eval.sh
+++ eval.sh
@@ -3,12 +3,12 @@ CUDA_VISIBLE_DEVICES=0
 b=regnet;
 for k in 2 3 5 8 1;
 do
-python -u eval.py --model log/old/$b/log_aoanet/model-best.pth \
-    --infos_path log/old/$b/log_aoanet/infos_aoanet.pkl \
+python -u eval.py --model log/old/$b/log_aoanet_rl/model-best.pth \
+    --infos_path log/old/$b/log_aoanet_rl/infos_aoanet.pkl \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
-    --name regnet$bSSHHHHHHHHHHHHH$k \
+    --name rlregnet$bSSHHHHHHHHHHHHH$k \
     --language_eval 1 \
     --beam_size $k \
     --batch_size 40 \
