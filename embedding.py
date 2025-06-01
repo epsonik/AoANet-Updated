22bf906b10787f2d0d2781846efcaff1c54b8027
mateuszb <mateusz.b@aibusters.com>
2025-06-02 01:22:56 +0200

    save checkpoints every iter

--- embedding.py
+++ embedding.py
@@ -74,7 +74,8 @@ def load_embeddings(
             # ignore word if not in train_vocab
             if emb_word not in vocab:
                 continue
-
+            print(emb_word)
+            print(vocab)
             embeddings[word_map[emb_word]] = torch.FloatTensor(embedding)
 
         # create cache file so we can load it quicker the next time
