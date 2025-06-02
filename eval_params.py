80787f3f43c1026fc8d960b094ce9d557dd8960c
mateuszb <mateusz.b@aibusters.com>
2025-06-02 23:49:28 +0200

    save checkpoints every iter

--- eval_params.py
+++ eval_params.py
@@ -17,8 +17,8 @@ def count_parameters(model, model_name):
     table = PrettyTable(["Modules", "Parameters"])
     total_params = 0
     for name, parameter in model.named_parameters():
-        if not parameter.requires_grad:
-            continue
+        # if not parameter.requires_grad:
+        #     continue
         params = parameter.numel()
         table.add_row([name, params])
         total_params += params
--- eval_params.sh
+++ eval_params.sh
@@ -13,4 +13,16 @@ python -u eval_params.py --model log/old/$b/log_aoanet/model-best.pth \
     --batch_size 40 \
     --split test \
     --cnn_model regnet16 ;
+
+python -u eval_params.py --model log/old/$b/log_aoanet_rl/model-best.pth \
+    --infos_path log/old/$b/log_aoanet/infos_aoanet.pkl \
+    --dump_images 0 \
+    --dump_json 1 \
+    --num_images -1 \
+    --name $b \
+    --language_eval 1 \
+    --beam_size 2 \
+    --batch_size 40 \
+    --split test \
+    --cnn_model regnet16 ;
 done
