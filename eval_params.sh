239ad24c01b347ba3e029b369818f97407c78e1b
mateuszb <mateusz.b@aibusters.com>
2025-06-02 15:52:56 +0200

    save checkpoints every iter

--- eval_params.sh
+++ eval_params.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0
-for b in regnet densenet121 densenet161 densenet201 inception resnet151 resnet101 bottom_up;
+for b in regnet densenet121 densenet161 densenet201 inception resnet152 resnet101 bottom_up;
 do
 python -u eval_params.py --model log/old/$b/log_aoanet_rl/model-best.pth \
     --infos_path log/old/$b/log_aoanet_rl/infos_aoanet.pkl \
