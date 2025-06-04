316ce915b09a9f6febbfa07da1a0d7e8a40adf3a
mateuszb <mateusz.b@aibusters.com>
2025-06-04 22:53:24 +0200

    save checkpoints every iter

--- eval_params.sh
+++ eval_params.sh
@@ -2,8 +2,8 @@
 CUDA_VISIBLE_DEVICES=0
 for b in regnet;
 do
-python -u eval_params.py --model log/old/$b/log_aoanet/model-best.pth \
-    --infos_path log/old/$b/log_aoanet/infos_aoanet.pkl \
+python -u eval_params.py --model log/old/regnet/log_aoanet/model-best.pth \
+    --infos_path log/old/regnet/log_aoanet/infos_aoanet.pkl \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
