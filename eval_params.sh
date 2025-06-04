55155fd4efd3d9bf73e4eaa902af086bf8fdbd67
mateuszb <mateusz.b@aibusters.com>
2025-06-04 22:54:12 +0200

    save checkpoints every iter

--- eval_params.sh
+++ eval_params.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0
-for b in regnet;
-do
+
 python -u eval_params.py --model log/old/regnet/log_aoanet/model-best.pth \
     --infos_path log/old/regnet/log_aoanet/infos_aoanet.pkl \
     --dump_images 0 \
@@ -13,4 +12,3 @@ python -u eval_params.py --model log/old/regnet/log_aoanet/model-best.pth \
     --batch_size 40 \
     --split test \
     --cnn_model regnet16 ;
-done
