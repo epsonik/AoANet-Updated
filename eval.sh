7c09d8b15eeab60c4bb74fb2f62ce9f6e33335cd
mateuszb <mateusz.b@aibusters.com>
2025-06-25 21:07:09 +0200

    get history

--- eval.sh
+++ eval.sh
@@ -2,12 +2,12 @@
 CUDA_VISIBLE_DEVICES=0
 for k in 2 3 5 8 1;
 do
-python -u eval.py --model log/old/densenet121/log_aoanet_rl/model-best.pth \
-    --infos_path log/old/densenet121/log_aoanet_rl/infos_aoanet-best.pkl \
+python -u eval.py --model log/old/densenet121/log_aoanet/model-best.pth \
+    --infos_path log/old/densenet121/log_aoanet/infos_aoanet-best.pkl \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
-    --name densenet121$k \
+    --name densenet121_f$k \
     --language_eval 1 \
     --beam_size $k \
     --batch_size 40 \
