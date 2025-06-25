85d36541decb29e432ef4429e0348fd0d1784c33
mateuszb <mateusz.b@aibusters.com>
2025-06-25 20:13:07 +0200

    get history

--- eval.sh
+++ eval.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0
-b=regnet;
 for k in 2 3 5 8 1;
 do
-python -u eval.py --model log/old/$b/log_aoanet_rl/model-best.pth \
-    --infos_path log/old/$b/log_aoanet_rl/infos_aoanet.pkl \
+python -u eval.py --model log/old/bottom_up/log_aoanet_rl/model-best.pth \
+    --infos_path log/old/bottom_up/log_aoanet_rl/infos_aoanet.pkl \
     --dump_images 0 \
     --dump_json 1 \
     --num_images -1 \
-    --name rlregnet$bSSHHHHHHHHHHHHH$k \
+    --name bottom_up$k \
     --language_eval 1 \
     --beam_size $k \
     --batch_size 40 \
