474bf386227a23c037ae5631634f92efa7982837
mateuszb <mateusz.b@aibusters.com>
2025-11-11 23:29:02 +0100

    inference

--- extract_features.sh
+++ extract_features.sh
@@ -1,3 +1,3 @@
-python scripts/prepro_feats.py --input_json '/mnt/dysk2/dane/coco2014/karpathy/dataset_coco.json' \
---output_dir data/cocotalk \
---images_root '/mnt/dysk2/dane/coco2014/'
+python scripts/prepro_feats.py --input_json '/home/bartosiewicz/mateusz/dane/coco2014/karpathy/cocotalk.json' --output_dir data/cocotalk --images_root '/home/bartosiewicz/mateusz/dane/coco2014'
+
+python scripts/prepro_labels.py --input_json '/home/bartosiewicz/mateusz/dane/coco2014/karpathy/dataset_coco.json' --output_json data/cocotalk.json --output_h5 data/cocotalk
---
+++ visualize.sh
@@ -0,0 +1,7 @@
+python visualize.py \
+    --model '/home/bartosiewicz/mateusz/densenet121/log_aoanet/model.pth' \
+    --infos_path '/home/bartosiewicz/mateusz/densenet121/log_aoanet/infos_aoanet.pkl' \
+    --image_folder '/home/bartosiewicz/mateusz/AoANet-Updated/vis/imgs' \
+    --output_dir 'vis/attention' \
+    --beam_size 3 \
+    --num_images 1
