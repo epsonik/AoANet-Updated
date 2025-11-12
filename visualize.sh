c8bdc8e21994b32217030951db908c06b9014aa3
mateuszb <mateusz.b@aibusters.com>
2025-11-12 01:19:08 +0100

    inference

--- visualize.sh
+++ visualize.sh
@@ -1,5 +1,5 @@
 python visualize.py \
-    --model '/home/bartosiewicz/mateusz/densenet121/log_aoanet/model.pth' \
-    --infos_path '/home/bartosiewicz/mateusz/densenet121/log_aoanet/infos_aoanet.pkl' \
+    --model '/home/bartosiewicz/mateusz/densenet121/log_aoanet/model-best.pth' \
+    --infos_path '/home/bartosiewicz/mateusz/densenet121/log_aoanet/infos_aoanet-best.pkl' \
     --image_folder '/home/bartosiewicz/mateusz/AoANet-Updated/vis/imgs' \
     --output_dir 'vis/attention'
