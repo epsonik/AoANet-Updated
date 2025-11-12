7d353789c5c594d981aff55eaf6bc2a4fec84021
mateuszb <mateusz.b@aibusters.com>
2025-11-12 01:26:23 +0100

    inference

--- visualize.sh
+++ visualize.sh
@@ -1,6 +1,6 @@
 python visualize.py \
     --model /home/bartosiewicz/mateusz/densenet201/log_aoanet/model-best.pth \
-    --infos_path /home/bartosiewicz/mateusz/densenet201/log_aoanet/infos-best.pkl \
+    --infos_path /home/bartosiewicz/mateusz/densenet201/log_aoanet/infos_aoanet-best.pkl \
     --cnn_model densenet201 \
     --image_folder vis/imgs \
     --num_images 1
