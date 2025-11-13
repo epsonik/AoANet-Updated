565eee82508f193fb72ac1e7ece27a78619db01e
mateuszb <mateusz.b@aibusters.com>
2025-11-13 15:23:54 +0100

    inference

--- dataloaderraw.py
+++ dataloaderraw.py
@@ -1,170 +1,66 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import json
 import os
-import sys
+from os.path import join
 
 import numpy as np
 import torch
-import skimage
-import skimage.io
-from torchvision import transforms as trn
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import transforms
 
-from misc.resnet_utils import myResnet
-
-preprocess = trn.Compose([
-    # trn.ToTensor(),
-    trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+from models.AttModel import MyResnet, MyDensenet
 
 
-class DataLoaderRaw():
+class DataLoaderRaw(Dataset):
 
     def __init__(self, opt):
         self.opt = opt
-        self.coco_json = opt.get('coco_json', '')
-        self.folder_path = opt.get('folder_path', '')
+        self.folder_path = opt.folder_path
+        self.image_list = self.get_image_list()
+        self.transforms = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
 
-        self.batch_size = opt.get('batch_size', 1)
-        self.seq_per_img = 1
-
-        print("start")
-        sys.path.append("./misc")
-
-        # Dynamically import and initialize the specified CNN model
-        cnn_model = opt.get('cnn_model', 'densenet161')
-        from densenet_utils import myDensenet
-
-        if cnn_model == 'densenet121':
-            from densenet import DenseNet121
-            net = DenseNet121()
-            self.feature_size = 1024
-        elif cnn_model == 'densenet161':
-            from densenet161 import DenseNet161
-            net = DenseNet161()
-            self.feature_size = 2208
-        elif cnn_model == 'densenet169':
-            from densenet169 import DenseNet169
-            net = DenseNet169()
-            self.feature_size = 1664
-        elif cnn_model == 'densenet201':
-            from densenet201 import DenseNet201
-            net = DenseNet201()
-            self.feature_size = 1920
-        elif cnn_model == 'regnet':
-            from regnet import RegNet16
-            net = RegNet16()
-            self.feature_size = 3024
-        elif cnn_model == 'inception':
-            from inception import Inception
-            net = Inception()
-            self.feature_size = 2048
-        else:  # Default to densenet161
-            from densenet161 import DenseNet161
-            net = DenseNet161()
-            self.feature_size = 2208
-            print(f"Warning: CNN model '{cnn_model}' not recognized. Defaulting to densenet161.")
-
-        self.my_densenet = myDensenet(net)
-        self.my_densenet.cuda()
-        self.my_densenet.eval()
-
-        # load the json file which contains additional information about the dataset
         print('DataLoaderRaw loading images from folder: ', self.folder_path)
+        print('listing all images in directory', self.folder_path)
 
-        self.files = []
-        self.ids = []
+        self.files = [f for f in os.listdir(self.folder_path)
+                      if os.path.isfile(join(self.folder_path, f))]
 
-        print(len(self.coco_json))
-        if len(self.coco_json) > 0:
-            print('reading from ' + opt.coco_json)
-            # read in filenames from the coco-style json file
-            self.coco_annotation = json.load(open(self.coco_json))
-            for k, v in enumerate(self.coco_annotation['images']):
-                fullpath = os.path.join(self.folder_path, v['file_name'])
-                self.files.append(fullpath)
-                self.ids.append(v['id'])
+        print('DataLoaderRaw found ', len(self.files), ' images')
+
+        if 'densenet' in self.opt.cnn_model:
+            self.cnn = MyDensenet(self.opt.cnn_model, pretrained=True)
         else:
-            # read in all the filenames from the folder
-            print('listing all images in directory ' + self.folder_path)
+            self.cnn = MyResnet(self.opt.cnn_model, pretrained=True)
 
-            def isImage(f):
-                supportedExt = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm', '.PPM']
-                for ext in supportedExt:
-                    start_idx = f.rfind(ext)
-                    if start_idx >= 0 and start_idx + len(ext) == len(f):
-                        return True
-                return False
+        self.cnn.to(torch.device('cpu'))
+        self.cnn.eval()
 
-            n = 1
-            for root, dirs, files in os.walk(self.folder_path, topdown=False):
-                for file in files:
-                    fullpath = os.path.join(self.folder_path, file)
-                    if isImage(fullpath):
-                        self.files.append(fullpath)
-                        self.ids.append(str(n))  # just order them sequentially
-                        n = n + 1
+    def get_image_list(self):
+        return
 
-        self.N = len(self.files)
-        print('DataLoaderRaw found ', self.N, ' images')
+    def get_image_path_list(self):
+        return self.files
 
-        self.iterator = 0
+    def __getitem__(self, index):
+        # Open image
+        img_path = join(self.folder_path, self.files[index])
 
-    def get_batch(self, split, batch_size=None):
-        batch_size = batch_size or self.batch_size
+        try:
+            img = self.transforms(Image.open(img_path).convert('RGB'))
+        except:
+            return None, None
 
-        # pick an index of the datapoint to load next
-        fc_batch = np.ndarray((batch_size, self.feature_size), dtype='float32')
-        att_batch = np.ndarray((batch_size, 14, 14, self.feature_size), dtype='float32')
-        max_index = self.N
-        wrapped = False
-        infos = []
+        img = img.reshape((1, 3, 224, 224))
 
-        for i in range(batch_size):
-            ri = self.iterator
-            ri_next = ri + 1
-            if ri_next >= max_index:
-                ri_next = 0
-                wrapped = True
-                # wrap back around
-            self.iterator = ri_next
+        with torch.no_grad():
+            tmp_att = self.cnn(img)
 
-            img = skimage.io.imread(self.files[ri])
+        return tmp_att, img_path
 
-            if len(img.shape) == 2:
-                img = img[:, :, np.newaxis]
-                img = np.concatenate((img, img, img), axis=2)
-
-            img = img[:, :, :3].astype('float32') / 255.0
-            img = torch.from_numpy(img.transpose([2, 0, 1])).cuda()
-            img = preprocess(img)
-            with torch.no_grad():
-                tmp_fc, tmp_att = self.my_densenet(img)
-
-            fc_batch[i] = tmp_fc.data.cpu().float().numpy()
-            att_batch[i] = tmp_att.data.cpu().float().numpy()
-
-            info_struct = {}
-            info_struct['id'] = self.ids[ri]
-            info_struct['file_path'] = self.files[ri]
-            infos.append(info_struct)
-
-        data = {}
-        data['fc_feats'] = fc_batch
-        data['att_feats'] = att_batch.reshape(batch_size, -1, self.feature_size)
-        data['att_masks'] = None
-        data['bounds'] = {'it_pos_now': self.iterator, 'it_max': self.N, 'wrapped': wrapped}
-        data['infos'] = infos
-
-        return data
-
-    def reset_iterator(self, split):
-        self.iterator = 0
-
-    def get_vocab_size(self):
-        return len(self.ix_to_word)
-
-    def get_vocab(self):
-        return self.ix_to_word
+    def __len__(self):
+        return len(self.files)
--- visualize.sh
+++ visualize.sh
@@ -3,8 +3,10 @@ python visualize.py \
     --infos_path /home/bartosiewicz/mateusz/resnet101/log_aoanet/infos_aoanet-best.pkl \
     --cnn_model resnet101 \
     --image_folder vis/imgs \
-    --num_images -1\
+    --num_images 0\
+    -- dump_images 1 \
     --batch_size 3\
+    -- dump_path 1 \
     --output_dir vis/resnet101_aoanet \
 
 python visualize.py \
@@ -12,6 +14,8 @@ python visualize.py \
     --infos_path /home/bartosiewicz/mateusz/resnet152/log_aoanet/infos_aoanet-best.pkl \
     --cnn_model resnet152 \
     --image_folder vis/imgs \
-    --num_images -1\
+    --num_images 0\
     --batch_size 3\
+    -- dump_images 1 \
+    -- dump_path 1 \
     --output_dir vis/resnet152_aoanet \
