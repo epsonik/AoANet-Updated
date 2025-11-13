f206af752824075882681814a0582bbba6a8c6a3
mateuszb <mateusz.b@aibusters.com>
2025-11-13 23:25:27 +0100

    inference

--- visualize.sh
+++ visualize.sh
@@ -1,8 +1,8 @@
 python visualize.py \
-    --model /home/bartosiewicz/mateusz/resnet101/log_aoanet/model-best.pth \
-    --infos_path /home/bartosiewicz/mateusz/resnet101/log_aoanet/infos_aoanet-best.pkl \
-    --cnn_model resnet101 \
+    --model /home/bartosiewicz/mateusz/resnet152/log_aoanet/model-best.pth \
+    --infos_path /home/bartosiewicz/mateusz/resnet152/log_aoanet/infos_aoanet-best.pkl \
+    --cnn_model resnet152 \
     --image_folder vis/imgs \
     --num_images -1 \
     --batch_size 3 \
-    --output_dir vis/resnet101_aoanet
+    --output_dir vis/resnet152_aoanet
