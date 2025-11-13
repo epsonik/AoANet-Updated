54ec03e6be4922b19d89f8cae5be7fc5545b2119
mateuszb <mateusz.b@aibusters.com>
2025-11-13 15:42:45 +0100

    inference

--- visualize.sh
+++ visualize.sh
@@ -1,8 +1,8 @@
 python visualize.py \
-    --model /home/bartosiewicz/mateusz/regnet/log_aoanet/model-best.pth \
-    --infos_path /home/bartosiewicz/mateusz/regnet/log_aoanet/infos_aoanet-best.pkl \
-    --cnn_model regnet \
+    --model /home/bartosiewicz/mateusz/resnet101/log_aoanet/model-best.pth \
+    --infos_path /home/bartosiewicz/mateusz/resnet101/log_aoanet/infos_aoanet-best.pkl \
+    --cnn_model resnet101 \
     --image_folder vis/imgs \
     --num_images -1 \
     --batch_size 3 \
-    --output_dir vis/regnet_aoanet
+    --output_dir vis/resnet101_aoanet
