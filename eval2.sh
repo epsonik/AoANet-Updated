f884b4280b1fde3c3098b7bd4eeaffdb32940484
mateuszb <mateusz.b@aibusters.com>
2025-11-17 23:32:56 +0100

    inference

--- eval2.sh
+++ eval2.sh
@@ -1,6 +1,6 @@
 python eval.py --model /home/bartosiewicz/mateusz/densenet121/log_aoanet/model-best.pth \
 --infos_path /home/bartosiewicz/mateusz/densenet121/log_aoanet/infos_aoanet-best.pkl \
---input_json /data/cocotalk.json \
+--input_json /home/bartosiewicz/mateusz/AoANet-Updated/data/cocotalk.json \
 --input_fc_dir /home/bartosiewicz/mateusz/AoANet-Updated/data/densenet121_fc \
 --input_att_dir /home/bartosiewicz/mateusz/AoANet-Updated/data/densenet121_att \
 --input_label_h5 none \
