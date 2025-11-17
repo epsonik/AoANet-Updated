c582fd2a1eb045d0bed8dc08d1d74497d372e5d3
mateuszb <mateusz.b@aibusters.com>
2025-11-17 22:03:25 +0100

    inference

---
+++ eval2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=3
+python eval.py --model /home/bartosiewicz/mateusz/log_aoanet/model-best.pth \
+    --infos_path /home/bartosiewicz/mateusz/log_aoanet/infos_aoanet-best.pkl \
+    --dump_json 1 \
+    --name single_eval_raw \
+    --beam_size 3 \
+    --batch_size 40 \
+    --cnn_model densenet121 \
+    --image_folder  /vis \
+    --coco_json eval_image.json
